c853db220d71789d9be339a39ba399119f09d682
Lucas Viga <lucasviga12@gmail.com>
2020-11-24 11:34:22 -0400

    send new notification when the applications is in status 3

--- index.js
+++ index.js
@@ -23,7 +23,33 @@ async function loadApplications() {
   console.log(filter);
 
   if(filter) {
-    console.log('enviar notificacao para:', filter[0].name);
+    const getAppName = filter[0].name;
+    const fullAppName = getAppName.split(' ');
+    var appName = fullAppName[1] ? getAppName.substr(getAppName.indexOf(' ') + 1) : '';
+
+    async function sendNotification(data) {
+      const response = await axios.post(
+        'https://onesignal.com/api/v1/notifications',
+        data, {
+          headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+            'Authorization': 'Basic OWQ0NDM3N2MtOWJjMS00NzAxLTliZDItMDE5M2MwMWE4YjEz',
+          },
+        },
+      );
+
+      console.log('** >> **', response.data);
+    }
+
+    var message = {
+      app_id: '0760b424-f6cd-4a7b-acc1-5ac06a997b9c',
+      contents: {'en': `Hmmm... No mmomento aplicacao ${appName} está passando por instabilidades. Saiba mais em: downdetector.com.br`},
+      included_segments: ['All'],
+    };
+
+    sendNotification(message);
+
+    console.log('enviar notificacao para:', appName);
   }
 
   if (filter.length <= 0) {
