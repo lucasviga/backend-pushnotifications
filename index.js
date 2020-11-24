92e38c96c20eb9f8b8ae6ab11f15f3b121285618
Lucas Viga <lucasviga12@gmail.com>
2020-11-24 17:22:49 -0400

    testando req a cada 1 minuto

--- index.js
+++ index.js
@@ -5,51 +5,66 @@ const apiURL = "http://monitor.clickip.com.br/api_jsonrpc.php";
 var statusDowndetector = [];
 
 async function loadApplications() {
-  const response = await axios.post(apiURL, {
-    jsonrpc: "2.0",
-    method: "item.get",
-    params: {
-        "output": "extend",
-        "hostids": "10084",
-        "sortfield": "name"
-    },
-    auth: "ea698574a0ecfc0fc7917744e2cdca22",
-    id: 1
-  });
 
-  statusDowndetector.push(response.data.result);
+  /**
+   * Every minute it is make a new request for
+   * check if some applications in status 3
+   */
 
-  const filter = statusDowndetector[0].filter(item => item.lastvalue === '3' && item.type !== '5');
-
-  if(filter) {
-    const getAppName = filter[0].name;
-    const fullAppName = getAppName.split(' ');
-    var appName = fullAppName[1] ? getAppName.substr(getAppName.indexOf(' ') + 1) : '';
-
-    async function sendNotification(data) {
-      const response = await axios.post(
-        'https://onesignal.com/api/v1/notifications',
-        data, {
-          headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-            'Authorization': 'Basic OWQ0NDM3N2MtOWJjMS00NzAxLTliZDItMDE5M2MwMWE4YjEz',
-          },
+  setInterval(async () => {
+    var date = new Date();
+    if ( date.getSeconds() === 0 ) {
+      const response = await axios.post(apiURL, {
+        jsonrpc: "2.0",
+        method: "item.get",
+        params: {
+            "output": "extend",
+            "hostids": "10084",
+            "sortfield": "name"
         },
-      );
+        auth: "ea698574a0ecfc0fc7917744e2cdca22",
+        id: 1
+      });
+
+      statusDowndetector.push(response.data.result);
+
+      const filter = statusDowndetector[0].filter(item => item.lastvalue === '3' && item.type !== '5');
+
+      console.log('uma nova requisicao', filter.length);
     }
+  }, 1000);
 
-    var message = {
-      app_id: '0760b424-f6cd-4a7b-acc1-5ac06a997b9c',
-      contents: {'en': `Hmmm... No mmomento aplicacao ${appName} está passando por instabilidades. Saiba mais em: downdetector.com.br`},
-      included_segments: ['All'],
-    };
+  // if(filter) {
+  //   for(x in filter) {
+  //     const getAppName = filter[x].name;
+  //     const fullAppName = getAppName.split(' ');
+  //     var appName = fullAppName[1] ? getAppName.substr(getAppName.indexOf(' ') + 1) : '';
 
-    sendNotification(message);
-  }
+  //     async function sendNotification(data) {
+  //       const response = await axios.post(
+  //         'https://onesignal.com/api/v1/notifications',
+  //         data, {
+  //           headers: {
+  //             'Content-Type': 'application/json; charset=utf-8',
+  //             'Authorization': 'Basic OWQ0NDM3N2MtOWJjMS00NzAxLTliZDItMDE5M2MwMWE4YjEz',
+  //           },
+  //         },
+  //       );
+  //     }
 
-  if (filter.length <= 0) {
-    return console.log('nenhuma aplicacao no nivel de atencao 3');
-  }
+  //     var message = {
+  //       app_id: '0760b424-f6cd-4a7b-acc1-5ac06a997b9c',
+  //       contents: {'en': `Hmmm... Possíveis problemas estão ocorrendo em ${appName}. Saiba mais em: downdetector.com.br`},
+  //       included_segments: ['All'],
+  //     };
+
+  //     sendNotification(message);
+  //   }
+  // }
+
+  // if (filter.length <= 0) {
+  //   return console.log('nenhuma aplicacao no nivel de atencao 3');
+  // }
 }
 
 loadApplications();
